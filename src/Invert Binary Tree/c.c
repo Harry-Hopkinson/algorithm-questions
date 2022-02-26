e12abc4b79936099b4749dfdb5b1ce5f9dc4bfce
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-02-26 19:51:17 +0000

    C.c

---
+++ src/Invert Binary Tree/c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if (root == NULL) {
+        return NULL;
+    }
+    struct TreeNode *temp = root->left;
+    root->left = invertTree(root->right);
+    root->right = invertTree(temp);
+    return root;
+}
